1c5ab3d6edac5ca76a71d8e5fae075657b65f756
buscotoxica <osvaldo.moya@sansano.usm.cl>
2026-04-21 15:57:58 -0400

    Add registrar_click path to logging app URLs

--- Plataforma_censo/logging_app/urls.py
+++ Plataforma_censo/logging_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views 
+
 app_name = 'logging'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='logging/panel.html'), name='panel'),
+    path("registrar-click/", views.registrar_click, name="registrar_click"),
 ]
